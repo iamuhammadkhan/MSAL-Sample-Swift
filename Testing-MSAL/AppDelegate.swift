6d89f7f7747ec04f10f395559243ea8ca3d316b1
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-08-24 16:53:23 +0500

    Example with swift completed

--- Testing-MSAL/AppDelegate.swift
+++ Testing-MSAL/AppDelegate.swift
@@ -6,14 +6,15 @@
 //
 
 import UIKit
+import MSAL
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        MSALGlobalConfig.loggerConfig.setLogCallback { (logLevel, message, containsPII) in
+            if (!containsPII) { print("%@", message!) }
+        }
         return true
     }
 
@@ -30,7 +31,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
-
+    
+    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        print("Received callback!")
+        guard let sourceApp = sourceApplication else { return false }
+        MSALPublicClientApplication.handleMSALResponse(url, sourceApplication: sourceApp)
+        return true
+    }
 }
-
