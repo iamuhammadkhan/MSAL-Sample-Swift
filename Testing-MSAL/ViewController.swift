ff5b97106274de0993879204b70687cb1974fec3
iamuhammadkhan <19494818+iamuhammadkhan@users.noreply.github.com>
2021-08-24 12:25:27 +0500

    Initial Commit

---
+++ Testing-MSAL/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Testing-MSAL
+//
+//  Created by Muhammad Khan on 8/24/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
